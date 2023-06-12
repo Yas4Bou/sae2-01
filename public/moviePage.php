125d9941e3b5f111b21fe32ec11d17853329040b
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-12 22:40:38 +0200

    creation de la premiere partie de la page movie

---
+++ public/moviePage.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+require_once '../vendor/autoload.php';
+use Database\MyPdo;
+use Html\WebPage;
+
+$moviePage = new WebPage();
+
+$moviePage ->appendContent("<header><h1>Films</h1></header>");
+
+if(isset($_GET["nombre"])){
+    $Id = $_GET['nombre'];
+
+    $requetes1 = MyPDO::getInstance()->prepare(
+        <<<'SQL'
+        SELECT id, title, releaseDate, originalTitle, overview , tagline
+        FROM movie
+        WHERE id = ? ;
+    SQL);
+
+    $requetes1 -> execute([$Id]);
+
+    while(($ligne = $requetes1->fetch()) !== false) {
+        $title = $moviePage->escapeString($ligne['title']);
+        $originTitle = $moviePage->escapeString($ligne['originalTitle']);
+        $tagline = $moviePage->escapeString($ligne['tagline']);
+        $overview = $moviePage->escapeString($ligne['overview']);
+        $moviePage -> setTitle(" Films - $title");
+        $moviePage->appendContent("<h1> Films - $title </h1> <p>$title- $ligne[releaseDate] <br> $originTitle <br> $overview <br> $tagline</p>");
+    }
+
+
+
+    $date = $moviePage ->getLastModification();
+    $moviePage ->appendContent("<footer>$date</footer>");
+
+}
+else{
+    header("Location : http://localhost:8000/homePage.php", true, 302);
+    exit(1);
+}
+echo $moviePage->TOHTML();
