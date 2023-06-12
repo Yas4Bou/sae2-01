384d1d2531db08c2a24fd2aee9367613203684eb
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-12 12:34:02 +0200

    creation du depot et mise en place du projet

---
+++ README.md
@@ -0,0 +1,5 @@
+# SAE 2.01 - developpement d'une application #
+
+# Auteurs #
+Martinet Line mart0421  
+Bouhriz-el-bouhlali Yasmina bouh0039
---
+++ src/Html/WebPage.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Html;
+
+class WebPage
+{
+    private string $head ;
+    private string $title ;
+    private string $body ;
+
+
+    /**
+     * @param string $title */
+    public function __construct(string $title = "")
+    {
+        $this->title = $title;
+        $this->head = "";
+        $this->body = "";
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getHead()
+    {
+        return $this->head;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+
+    /**
+     * @param string $content
+     * @return void
+     */
+    public function appendToHead(string $content): void
+    {
+        $this->head .= $content;
+    }
+
+
+    /**
+     * @param string $css
+     * @return void
+     */
+    public function appendCss(string $css): void
+    {
+        $this->head .= "<style>\n $css \n </style>";
+    }
+
+
+    /**
+     * @param string $url
+     * @return void
+     */
+    public function appendCssUrl(string $url): void
+    {
+        $this->head.= "<link rel=\"stylesheet\" href=\"$url\"/>";
+
+    }
+
+
+    /**
+     * @param string $js
+     * @return void
+     */
+    public function appendJs(string $js): void
+    {
+        $this->head.= "<script> $js </script>";
+
+    }
+
+
+    /**
+     * @param string $url
+     * @return void
+     */
+    public function appendJsUrl(string $url): void
+    {
+        $this->head .= "<script src=\"$url\"></script>";
+    }
+
+
+    /**
+     * @param string $content
+     * @return void
+     */
+    public function appendContent(string $content): void
+    {
+        $this->body.= $content;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function TOHTML(): string
+    {
+        $text = "<!doctype html>\n<html lang=\"fr\">\n";
+        $text .= "<head>\n";
+        $text .= "<meta charset='UTF-8' name='viewport'>\n";
+        $text .= "<title>\n$this->title\n</title>";
+        $text .= "\n$this->head\n</head>\n";
+        $text .= "<body>\n$this->body\n</body>\n</html>";
+        return $text;
+    }
+
+
+    /**
+     * @param string $string
+     * @return string
+     */
+    public function escapeString(string $string): string
+    {
+        return htmlspecialchars($string, ENT_QUOTES|ENT_HTML5);
+
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function getLastModification(): string
+    {
+        return date(getlastmod());
+    }
+
+
+
+
+}
