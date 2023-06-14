852163e13214e5163cc394554c0cbe37be67fe19
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 18:48:00 +0200

    ajout de la methode static findAll()

--- src/Entity/movie_cast.php
+++ src/Entity/movie_cast.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Excpetion;
+
 class movie_cast
 {
     private int $id ;
@@ -44,6 +47,35 @@ class movie_cast
             $this->avatarId = 0;
         }
         return $this->avatarId;
+    }
+
+    /**
+     * Cette methode retourne un tableau qui contient tous les Acteurs et leurs role de la base de données, dans l'ordre alphabetique
+     * @return \Entity\movie_cast[]
+     */
+    public static function findAll(string $Id) : movie_cast
+    {
+        $requete =  MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT p.id , name , role , avatarId
+            FROM people p , cast c , movie m 
+            WHERE m.id = c.movieId 
+                AND c.peopleId = p.id 
+                AND m.id = ?
+            ORDER BY role ;
+        SQL);
+
+
+        $requete -> execute([$Id]);
+        $requete -> setFetchMode(MyPdo::FETCH_CLASS, \Entity\movie_cast::class);
+        $tab = $requete ->fetch();
+
+        if(!$tab){
+            throw new Excpetion\EntityNotFoundException("id : {$Id} Cet acteur n'existe pas ");
+        }
+        else {
+            return $tab;
+        }
     }
 
 
