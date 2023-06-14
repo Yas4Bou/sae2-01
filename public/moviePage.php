56a047318a46eb8f7a4d04c1d2ddcd059f7d61e9
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 18:47:19 +0200

    ajout de l'utilisation de la methode statis findAll()

--- public/moviePage.php
+++ public/moviePage.php
@@ -3,11 +3,9 @@ declare(strict_types=1);
 
 require_once '../vendor/autoload.php';
 
-use Entity\movie;
-
 use Css\AppWebPage;
-use Entity\movieCollectionRequete1;
-use Entity\movieCollectionRequete2;
+use Entity\movie; 
+use Entity\movie_cast;
 
 $moviePage = new AppWebPage();
 
@@ -35,21 +33,20 @@ if(isset($_GET["nombre"])){
                                     </div>
                                    </nav>");
 
-   $requetes2 = new movieCollectionRequete2();
-   $tableau2 = $requetes2->findAll($Id);
+   $value = movie_cast::findAll($Id);
 
 
-    foreach ($tableau2  as $key => $value) {
-        $name = $moviePage->escapeString($value->getName());
-        $role = $moviePage->escapeString($value->getRole());
-        $avatarId = $value ->getAvatarId();
-        $id = $value->getId();
-        $moviePage->appendContent("<div class='main'>
+
+   $name = $moviePage->escapeString($value->getName());
+   $role = $moviePage->escapeString($value->getRole());
+   $avatarId = $value ->getAvatarId();
+   $id = $value->getId();
+   $moviePage->appendContent("<div class='main'>
                                <img src= 'image.php?imageID=$avatarId' width='100px' height='150px'>
                                <article class='main__item'><a href='http://localhost:8000/actorPage.php?nombre=$id'>Role : $role</a></article>
                                <article class='main__item'><a href='http://localhost:8000/actorPage.php?nombre=$id'>Vrai nom : $name</a></article>
                            </div>");
-   }
+
 
 
    $date = $moviePage ->getLastModification();
