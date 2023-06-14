36fc64fabd6d7015ed24adf6ae201308ed426cf7
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-06-14 13:46:07 +0200

    Ajout de l'appel à la méthode appendCssUrl()

--- public/homePage.php
+++ public/homePage.php
@@ -23,63 +23,7 @@ $homePage->appendContent("</div>");
 $date = $homePage ->getLastModification();
 $homePage ->appendContent("<footer>$date</footer>");
 
-$css = "
-body {
-  display: flex;
-  flex-direction: column;
-  justify-content: stretch;
-  gap: 0.5em 2em;
-  background-color: black;
-  color:white;
-}
-
-.movie {
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    align-content: space-around;
-    border: 1px solid white;
-    padding: 10px;
-    gap: 1em;
-}
-
-.movie-items {
-    display: flex;
-    align-items: center;
-    width: 6em;
-    flex-shrink: 0;
-    border: 1px solid white;
-    padding: 10px;
-}
-a {
-  color: red;
-}
-
-a:hover {
-  color: white;
-}
-
-header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid white;
-    padding: 10px;
-    margin-bottom: 20px;
-    color: red;
-}
-
-footer {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: 1px solid white;
-    padding: 10px;
-    
-
-}
-";
-$homePage -> appendCss($css);
+$homePage->appendCssUrl("css/HomeCss.css");
 
 
 echo $homePage->TOHTML();
