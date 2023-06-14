cfb45e757a9b177292b6d85c4207e244348b7e10
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-06-14 04:27:13 +0200

    Ajout de la partie css à HomePage.php et ajout de 2 classes (html): .movie-items et movie

--- public/homePage.php
+++ public/homePage.php
@@ -10,14 +10,15 @@ $homePage = new AppWebPage('Film');
 $requete = new homeCollection();
 $tableau = $requete ->findAll();
 
+$homePage->appendContent("<div class='movie'>");
 
 foreach ($tableau  as $key => $value) {
     $l = $homePage->escapeString($value ->getTitle());
     $id = $value ->getId();
-    $homePage->appendContent("<a href='http://localhost:8000/moviePage.php?nombre=$id'>$l</a></div>");
+    $homePage->appendContent("<div class='movie-items'><a href='http://localhost:8000/moviePage.php?nombre=$id'>$l</a></div>");
 }
 
-
+$homePage->appendContent("</div>");
 $date = $homePage ->getLastModification();
 $homePage ->appendContent("<footer>$date</footer>");
 
@@ -31,6 +32,25 @@ body {
   color:white;
 }
 
+.movie {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    align-content: space-around;
+    border: 1px solid white;
+    padding: 10px;
+    gap: 1em;
+}
+
+.movie-items {
+    display: flex;
+    align-items: center;
+    width: 6em;
+    flex-shrink: 0;
+    border: 1px solid white;
+    padding: 10px;
+}
+
 header {
     display: flex;
     justify-content: center;
@@ -40,7 +60,6 @@ header {
     margin-bottom: 20px;
     color: red;
 }
-
 
 footer {
     display: flex;
