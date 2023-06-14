3ac221615dfac63753d3ff918180c66de5f3aa88
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-06-14 21:30:52 +0200

    Ajout du constructeur, et du getter à la classe ActorForm

--- src/Html/Form/ActorForm.php
+++ src/Html/Form/ActorForm.php
@@ -2,8 +2,27 @@
 declare(strict_types=1);
 
 namespace Html\Form;
+use Entity\actor;
 
 class ActorForm
 {
+    private ?Actor $actor;
+
+    /**
+     * @param actor|null $actor
+     */
+    public function __construct(?actor $actor)
+    {
+        $this->actor = $actor;
+    }
+
+    /**
+     * @return actor|null
+     */
+    public function getActor(): ?actor
+    {
+        return $this->actor;
+    }
+
 
 }
