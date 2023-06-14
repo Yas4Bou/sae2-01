1697de71a9f818af34d2ae59d7b05167f2dad2ba
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 12:17:03 +0200

    creation de la page image.php pour l'importation des images

---
+++ public/image.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+use Entity\cover;
+use Entity\Exception\EntityNotFoundException;
+use Exception\ParameterException;
+
+try {
+    ///////////////////////
+    // À vous de jouer ! //
+    ///////////////////////
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
+if(isset($_GET['imageID'])){
+    $Id = $_GET['imageID'];
+    $image= new cover();
+    $image = cover::findById($Id);
+    header("Content-Type : image/jpeg");
+    echo $image->getJpeg();
+    }
+else {
+    throw new \Entity\Excpetion\ParameterException();
+}
+
