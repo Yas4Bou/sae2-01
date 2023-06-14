aefed889d674762abdc0ec910fbeba84b9d06670
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 16:33:22 +0200

    suppresion de la classe actorCollectionRequete2.php qui ne sert plus a rien suite a l'ajout de la methodestic findAll() dans actor_cast_movie.php

--- public/actorPage.php
+++ public/actorPage.php
@@ -1,14 +1,15 @@
 <?php
+
 declare(strict_types=1);
 require_once '../vendor/autoload.php';
 use Database\MyPdo;
 use Css\AppWebPage;
 use Entity\actorCollectionRequete1;
-use Entity\actorCollectionRequete2;
+use Entity\actor_cast_movie;
 
 $actorPage = new AppWebPage();
 
-if(isset($_GET["nombre"])){
+if(isset($_GET["nombre"])) {
     $Id = $_GET['nombre'];
 
     $requetes1 = new actorCollectionRequete1();
@@ -36,17 +37,15 @@ if(isset($_GET["nombre"])){
                                     </div>");
     }
 
+    $value = actor_cast_movie::findAll($Id);
 
-    $requetes2 = new actorCollectionRequete2();
-    $tableau2 = $requetes2 ->findAll($Id);
 
-    foreach ($tableau2 as $key => $value) {
-        $title = $actorPage->escapeString($value->getTitle());
-        $role = $actorPage->escapeString($value->getRole());
-        $id = $value->getId();
-        $releaseDate = $value->getReleaseDate();
-        $posterId = $value->getPosterId();
-        $actorPage->appendContent("<div class='conteneur'> 
+    $title = $actorPage->escapeString($value->getTitle());
+    $role = $actorPage->escapeString($value->getRole());
+    $id = $value->getId();
+    $releaseDate = $value->getReleaseDate();
+    $posterId = $value->getPosterId();
+    $actorPage->appendContent("<div class='conteneur'> 
                                     <img src= 'image.php?imageID=$posterId' width='100px' height='150px'>
                                     <div class='film'>
                                         <div class='title'><a href='http://localhost:8000/moviePage.php?nombre=$id'> Titre du film : $title </a> </div>
@@ -56,12 +55,11 @@ if(isset($_GET["nombre"])){
                                         <div class='role_item'><a href='http://localhost:8000/moviePage.php?nombre=$id'> Role : $role  </a> </div>
                                       </div>
                                     </div>");
-    }
+
 
     $date = $actorPage ->getLastModification();
     $actorPage ->appendContent("<footer>$date</footer>");
-}
-else{
+} else {
     header("Location : http://localhost:8000/homePage.php", true, 302);
     exit(1);
 }
