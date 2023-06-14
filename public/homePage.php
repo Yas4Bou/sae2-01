3b5cb385a415099266ca17b1f0331a35da274f2e
Yasmina Bouhriz El Bouhlali <yasmina.bouhriz-el-bouhlali@etudiant.univ-reims.fr>
2023-06-14 04:24:23 +0200

    Ajout de la partie css à HomePage.php: body, header et footer

--- public/homePage.php
+++ public/homePage.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 require_once '../vendor/autoload.php';
 
-use Database\MyPdo;
 use Css\AppWebPage;
 use Entity\homeCollection;
 
@@ -11,13 +10,49 @@ $homePage = new AppWebPage('Film');
 $requete = new homeCollection();
 $tableau = $requete ->findAll();
 
+
 foreach ($tableau  as $key => $value) {
     $l = $homePage->escapeString($value ->getTitle());
-    $id = $value ->getId(); 
-    $homePage->appendContent("<a href='http://localhost:8000/moviePage.php?nombre=$id '>$l</a><br>");
+    $id = $value ->getId();
+    $homePage->appendContent("<a href='http://localhost:8000/moviePage.php?nombre=$id'>$l</a></div>");
 }
+
 
 $date = $homePage ->getLastModification();
 $homePage ->appendContent("<footer>$date</footer>");
+
+$css = "
+body {
+  display: flex;
+  flex-direction: column;
+  justify-content: stretch;
+  gap: 0.5em 2em;
+  background-color: black;
+  color:white;
+}
+
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    padding: 10px;
+    margin-bottom: 20px;
+    color: red;
+}
+
+
+footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    padding: 10px;
+    
+
+}
+";
+$homePage -> appendCss($css);
+
 
 echo $homePage->TOHTML();
