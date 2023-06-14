2f9bfc5fef6de66b7285a4112defb64230aa56ed
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 15:12:34 +0200

    ajout de l'instance  pour les images des acteurs dans moviePage.php

--- src/Entity/movie_cast.php
+++ src/Entity/movie_cast.php
@@ -8,6 +8,7 @@ class movie_cast
 {
     private int $id ;
     private string $name ;
+    private int|null $avatarId;
     private string $role;
 
     /**
@@ -32,6 +33,17 @@ class movie_cast
     public function getRole(): string
     {
         return $this->role;
+    }
+    /**
+    * @return int
+    */
+    public function getAvatarId(): int
+    {
+        if (is_null($this->avatarId))
+        {
+            $this->avatarId = 0;
+        }
+        return $this->avatarId;
     }
 
 
