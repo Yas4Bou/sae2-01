23fcdc1f4682b30e61e268ba772e25d6a564ef53
line martinet <line.martinet@etudiant.univ-reims.fr>
2023-06-14 17:10:36 +0200

    ajout de la mathode static findAll()

--- src/Entity/actor.php
+++ src/Entity/actor.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Excpetion;
 class actor
 {
     private string|null $death;
@@ -84,7 +86,30 @@ class actor
         return $this->death;
     }
 
+    /**
+     * Cette methode retourne un tableau qui contient toutes les information des acteurs de la base de données
+     * @return \Entity\actor[]
+     */
+    public static function findAll( string $Id) : actor
+    {
+        $requete =  MyPDO::getInstance()->prepare(
+            <<<'SQL'
+            SELECT id, name, placeOfBirth, birthday, deathday as death, biography , avatarId
+            FROM people
+            WHERE id = ? ;
+        SQL);
 
+        $requete -> execute([$Id]);
+        $requete -> setFetchMode(MyPdo::FETCH_CLASS, actor::class);
+        $tab = $requete ->fetch();
+
+        if(!$tab){
+            throw new Excpetion\EntityNotFoundException("id : {$Id} Cet acteur n'existe pas ");
+        }
+        else {
+            return $tab;
+        }
+    }
 
 
 }
